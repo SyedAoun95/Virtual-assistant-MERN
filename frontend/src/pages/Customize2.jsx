28ba1e47e6507f9d997fff9eed95029888c338d0
Syedaoun95 <aouns6229@gmail.com>
2025-07-22 09:52:09 +0500

    created customize2.jsx

--- frontend/src/pages/Customize2.jsx
+++ frontend/src/pages/Customize2.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { userDataContext } from '../context/userContext'
 
 function Customize2() {
+  const {userData}=useContext(userDataContext)
+  const[AssistantName,setAssistantName]=useState(userData?.AssistantName || "")
   return (
-    <div>
-      
+    <div className='w-full h-[100vh] bg-gradient-to-t from-[black] to-[#030353] flex justify-center items-center flex-col'>
+       <h1 className='text-white text-[30px] text-center p-[20px] mb-[10px]'>Select your <span className='text-blue-300'>Assistant name</span></h1>
+        <input type="text" placeholder='eg . Roberto' className='w-[350px] h-[50px] lg:w-[700px] lg:h-[60px] outline-none border-2 border-white bg-transparent text-white placeholder-gray-300 px-[20px] py-[10px] rounded-full text-[18px] '  />
+         <button className='min-w-[300px] mt-[30px] h-[60px] text-[18px] bg-white text-black font-semibold rounded-full cursor-pointer'required onChange={(e)=>setAssistantName(e.target.value)} value={AssistantName}>Finally Create your Assistant</button>
     </div>
+    
   )
 }
 
