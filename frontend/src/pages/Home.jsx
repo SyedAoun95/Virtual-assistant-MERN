704b852856e1ae0bbf72eab495918b4400ec1d4a
Syedaoun95 <aouns6229@gmail.com>
2025-07-23 11:16:57 +0500

    update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'; 
+import { userDataContext } from "../context/userContext";
 
 function Home() {
-  
+  const { userData } = useContext(userDataContext); 
+
   return (
-     <div className='w-full h-[100vh] bg-gradient-to-t from-[black] to-[#050546] flex justify-center items-center flex-col'></div>
-  )
+    <div className='w-full h-[100vh] bg-gradient-to-t from-[black] to-[#050546] flex justify-center items-center flex-col'>
+      
+    </div>
+  );
 }
 
-export default Home
+export default Home;
